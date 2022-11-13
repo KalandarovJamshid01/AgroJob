fb562ce4fdc5d49b69fb7e895cc9870550db6d8b
Jamshid <kalandarovjamshid01@gmail.com>
2022-11-13 19:56:48 +0500

    deleteJob is tready

--- public/assets/js/myjobs.js
+++ public/assets/js/myjobs.js
@@ -1,3 +1,29 @@
-const deleteJob = document.querySelector(".deleteJob");
+const deleteJobBtn = document.querySelector(".deleteJob");
 
+const deleteJob = async (jobId) => {
+  try {
+    const res = await axios({
+      method: "DELETE",
+      url: `http://127.0.0.1:8000/api/v1/jobs/${jobId}`,
+    });
+    if (res.status === 204) {
+      alert("Job deleteded successfully");
+      window.setTimeout(() => {
+        location.reload();
+      }, 1000);
+    } else {
+      alert("Xatolik yuz berdi");
+      window.setTimeout(() => {
+        location.reload();
+      }, 1000);
+    }
+  } catch (err) {
+    console.log(err.response);
+  }
+};
 
+deleteJobBtn.addEventListener("click", () => {
+  const jobId = deleteJobBtn.value;
+  console.log(jobId);
+  deleteJob(jobId);
+});
